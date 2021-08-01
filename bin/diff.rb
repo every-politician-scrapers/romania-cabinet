7540a4c0d7fcba7c9b383f1b6e6bff44b87a5bf1
Tony Bowden <tony@tmtm.com>
2021-08-01 20:08:20 +0300

    Compare sources case-insensitively
    I still can't work out how to get `daff` to ignore case, so coerce
    everything to lowercase before comparing.

--- bin/diff.rb
+++ bin/diff.rb
@@ -2,6 +2,18 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/comparison'
+require 'pry'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
-puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
+# Process the data from each source before comparison
+class Comparison < EveryPoliticianScraper::Comparison
+  def wikidata_csv_options
+    { converters: [->(val) { val.to_s.downcase }] }
+  end
+
+  def external_csv_options
+    { converters: [->(val) { val.to_s.downcase }] }
+  end
+end
+
+diff = Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
