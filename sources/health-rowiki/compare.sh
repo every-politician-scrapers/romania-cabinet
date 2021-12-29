d9234f9110735b598335cd301e69d7bec028f5a5
Tony Bowden <tony@tmtm.com>
2021-12-29 17:41:12 +0200

    Change into correct directory for health ministers

--- sources/health-rowiki/compare.sh
+++ sources/health-rowiki/compare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd sources/agriculture-rowiki/
+cd sources/health-rowiki/
 
 bundle exec ruby scraper.rb | ifne tee scraped.csv
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | ifne tee wikidata.csv
